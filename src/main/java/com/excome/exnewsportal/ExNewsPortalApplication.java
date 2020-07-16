f7bdce43402339b7a10b0d96d6d9b09c38f08cb7
Excome <56866949+Excome@users.noreply.github.com>
2020-07-16 16:26:04 +0300

    get started

---
+++ src/main/java/com/excome/exnewsportal/ExNewsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.excome.exnewsportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExNewsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExNewsPortalApplication.class, args);
+	}
+
+}
